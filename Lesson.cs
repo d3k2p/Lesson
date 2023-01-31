987c91b068927f0b469fcdb6872f42118601c909
PawelBoB <118815705+PawelBoB@users.noreply.github.com>
2023-01-31 23:10:25 +0300

    Add files via upload

---
+++ Lesson.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace myNameSpace;
+class Lesson
+{
+    private object linkSubject, linkTeacher, linkAuditory, linkGroup, linkLesson;
+    private DateTime? date;
+    Lesson(object linkSubject, object linkTeacher, object linkAuditory, object linkGroup, object linkLesson, DateTime? date = null)
+    {
+        this.linkSubject = linkSubject;
+        this.linkTeacher = linkTeacher;
+        this.linkAuditory = linkAuditory;
+        this.linkGroup = linkGroup;
+        this.linkLesson = linkLesson;
+        this.date = date != null ? date : DateTime.Now;
+    }
+}
