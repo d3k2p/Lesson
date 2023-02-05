738b677e6cd498481cb7168483bcf4258e1e4d11
PawelBoB <118815705+PawelBoB@users.noreply.github.com>
2023-02-05 21:30:26 +0300

    Update Theme_LessonType.cs

--- base/base/src/Theme_LessonType.cs
+++ base/base/src/Theme_LessonType.cs
@@ -3,7 +3,13 @@ using System;
 
 class Theme_LessonType
 {
-    public Theme_LessonType()
+    private Theme theme;
+    private LessonType lessonType;
+    private UInt16 hours;
+    public Theme_LessonType(Theme theme, LessonType lessonType, UInt16 hours)
     {
+        this.theme = theme;
+        this.lessonType = lessonType;
+        this.hours = hours;
     }
 }
