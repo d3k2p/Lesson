cd9d8467f7adc41ebdebf74e15a77eb58e549c1c
Str1lok <118817953+Str1lok@users.noreply.github.com>
2023-02-05 21:16:13 +0300

    Update and rename base/base/src/Post.cs to База/База/Src/Post.cs

---
+++ База/База/Src/Post.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualBasic;
+using System;
+
+class Post
+{
+    private string name;
+    private int salary;
+    public Post(sting name, int salary)
+    {
+        this.name = name;
+        this.salary = salary;
+    }
+}
