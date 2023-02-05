273b796fa7d327b0ebea068f0412f23680a5487a
PawelBoB <118815705+PawelBoB@users.noreply.github.com>
2023-02-05 21:17:39 +0300

    Update Lesson.cs
    bob

--- base/base/src/Lesson.cs
+++ base/base/src/Lesson.cs
@@ -3,7 +3,17 @@ using System;
 
 class Lesson
 {
-    public Lesson()
+    private Discipline discipline;
+    private Employee employee;
+    private Group group;
+    private Pair pair;
+    private LessonType lessonType;
+    public Lesson(Discipline discipline, Employee employee, Group group, Pair pair, LessonType lessonType)
     {
+        this.discipline = discipline;
+        this.employee = employee;
+        this.group = group;
+        this.pair = pair;
+        this.lessonType = lessonType;
     }
 }
