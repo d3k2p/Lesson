d3ae92d6c3cefeacdcb90ed9e3377d49e873af47
gigilya <118998483+gigilya@users.noreply.github.com>
2023-02-06 20:20:01 +0300

    Update Pair.cs

--- base/base/src/Pair.cs
+++ base/base/src/Pair.cs
@@ -2,8 +2,18 @@ using Microsoft.VisualBasic;
 using System;
 
 class Pair
-{
-    public Pair()
+    
+{   private TimeOnly beginningsP;
+    private TimeOnly endingP;
+    private TimeOnly beginningsBreak;
+    private TimeOnly endingBreak;
+    private Shift shift;
+    
+    public Pair (TimeOnly beginningsP,TimeOnly endingP,TimeOnly beginningsBreak,TimeOnly endingBreak)
     {
+        this.TimeOnly beginningsP = beginningsP
+        this.TimeOnly endingP = endingP
+        this.TimeOnly beginningsBreak = beginningsBreak
+        this.TimeOnly endingBreak = endingBreak
     }
 }
