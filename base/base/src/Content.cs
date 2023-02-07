12e526106f70a1ec15aa9307583b6755036ca93e
d3k2p <118817036+d3k2p@users.noreply.github.com>
2023-02-07 21:25:14 +0300

    Update Content.cs

--- base/base/src/Content.cs
+++ base/base/src/Content.cs
@@ -3,7 +3,12 @@ using System;
 
 class Content
 {
+    private string name;
+    private string author;
+    
     public Content()
     {
+        this.name = name;
+    	this.author = author;
     }
 }
